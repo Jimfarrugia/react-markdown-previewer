fa2a6be16b7ad9da0e6078a98a117b15f5429b70
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-12 00:30:46 +1100

    layout responsive design with html/css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 function App() {
 	return (
-		<div id="container">
-			<div id="editorContainer">Hello</div>
-			<div id="previewContainer">Hello</div>
+		<div class="container">
+			<header>
+				<h1>Markdown Previewer</h1>
+			</header>
+			<div class="grid">
+				<div class="input">
+					<textarea>
+						## Preview your Markdown Lorem ipsum dolor sit amet, consectetur
+						adipiscing elit. Integer convallis quis est et mollis. Suspendisse
+						potenti. Sed malesuada vehicula rutrum. Curabitur hendrerit odio nec
+						vulputate porta. Aliquam vitae nunc vel felis feugiat vulputate ac
+						in ante. Proin faucibus libero et tempus viverra. Pellentesque et
+						lectus pharetra, dictum justo sed, euismod nulla.
+					</textarea>
+				</div>
+				<div class="preview">
+					<h2>Preview your Markdown</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+						convallis quis est et mollis. Suspendisse potenti. Sed malesuada
+						vehicula rutrum.
+					</p>
+					<p>
+						Curabitur hendrerit odio nec vulputate porta. Aliquam vitae nunc vel
+						felis feugiat vulputate ac in ante. Proin faucibus libero et tempus
+						viverra. Pellentesque et lectus pharetra, dictum justo sed, euismod
+						nulla.
+					</p>
+				</div>
+			</div>
+			<footer>
+				<ul>
+					<li>
+						<a href="#">Markdown Reference</a>
+					</li>
+					<li>
+						<a href="#">Source Code</a>
+					</li>
+					<li>
+						<a href="#">Jim Farrugia</a>
+					</li>
+				</ul>
+			</footer>
 		</div>
 	);
 }
