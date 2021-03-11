797758dad6cbd2fa0cbe2fa7b7da117d6e0f710c
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-12 01:10:56 +1100

    control Input's textarea content with state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import Preview from "./components/Preview";
 import Footer from "./components/Footer";
 
+const initialContent =
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis quis est et mollis. Suspendisse potenti. Sed malesuada vehicula rutrum. Curabitur hendrerit odio nec vulputate porta. Aliquam vitae nunc vel felis feugiat vulputate ac in ante. Proin faucibus libero et tempus viverra. Pellentesque et lectus pharetra, dictum justo sed, euismod nulla.";
+
 function App() {
+	const [content, setContent] = useState(initialContent);
+
 	return (
 		<div className="container">
 			<Header title="Markdown Previewer" />
 			<div className="grid">
-				<Input />
+				<Input content={content} setContent={setContent} />
 				<Preview />
 			</div>
 			<Footer />
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,11 @@
-const Input = () => {
+const Input = ({ content, setContent }) => {
 	return (
 		<div className="input">
-			<textarea id="editor">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-				convallis quis est et mollis. Suspendisse potenti. Sed malesuada
-				vehicula rutrum. Curabitur hendrerit odio nec vulputate porta. Aliquam
-				vitae nunc vel felis feugiat vulputate ac in ante. Proin faucibus libero
-				et tempus viverra. Pellentesque et lectus pharetra, dictum justo sed,
-				euismod nulla.
-			</textarea>
+			<textarea
+				id="editor"
+				value={content}
+				onChange={e => setContent(e.target.value)}
+			/>
 		</div>
 	);
 };
